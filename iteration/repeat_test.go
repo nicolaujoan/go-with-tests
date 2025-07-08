44993b14c987e866f049bf1b28c85d91e87b4053
jnicolau <jnicolau@riu.com>
2025-07-08 11:21:44 +0200

    iteration done

--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -1,10 +1,23 @@
 package iteration
 
-import "testing"
+import (
+	"testing"
+	"fmt"
+	"strings"
+)
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a")
-	expected := "aaaaa"
+	repeated := Repeat("a", 2)
+	expected := "aa"
+
+	if repeated != expected {
+		t.Errorf("expected %q but got %q", expected, repeated)
+	}
+}
+
+func TestRepeatStdLib(t *testing.T) {
+	repeated := strings.Repeat("a", 2)
+	expected := "aa"
 
 	if repeated != expected {
 		t.Errorf("expected %q but got %q", expected, repeated)
@@ -15,6 +28,19 @@ func TestRepeat(t *testing.T) {
 // go test -bench=.
 func BenchmarkRepeat(b *testing.B) {
 	for b.Loop() {
-		Repeat("a")
+		Repeat("a", 2)
 	}
+}
+
+func BenchmarkRepeatStdLib(b *testing.B) {
+	for b.Loop() {
+		strings.Repeat("a", 2)
+	}
+}
+
+// the output comment is needed to execute the example
+func ExampleRepeat() {
+	repeated := Repeat("a", 2)
+	fmt.Println(repeated)
+	// Output: aa
 }
